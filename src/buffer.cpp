7219191acedf45139b8dd643d3d7a5b69e6aa2ab
Ayman Alzomaili <ayman@rockhopper-05.cs.wisc.edu>
2020-11-01 18:11:34 -0600

    added name and id

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -3,6 +3,7 @@
  * CS 564 Group 55
  * James Ma: 9079648441
  * Keyi Wang: 9080306518
+ * Ayman Alzomaili: 9075944141
  *
  * @author See Contributors.txt for code contributors and overview of BadgerDB.
  *
